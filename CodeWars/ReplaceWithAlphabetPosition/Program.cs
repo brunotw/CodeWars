83c89ae3fc48ad1f785d8ccad7aa411d6cb3fa09
Bruno Willian <bruno.willian.ads@gmail.com>
2019-09-09 15:51:53 +0100

    ReplaceWithAlphabetPosition

---
+++ CodeWars/ReplaceWithAlphabetPosition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ReplaceWithAlphabetPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "the sunset sets at twelve o' clock.";
+            var r = String.Join(" ", text.ToCharArray().Where(Char.IsLetter).Select(c => (int)c % 32));
+            r.Trim();
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
